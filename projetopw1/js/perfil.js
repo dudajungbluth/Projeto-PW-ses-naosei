aeecb05f91e415b41b9a138a4f419d4360a0a54b
Maria Eduarda Teixeira Jungbluth <129319061+dudajungbluth@users.noreply.github.com>
2023-11-09 07:05:43 -0300

    Add files via upload

---
+++ projetopw1/js/perfil.js
@@ -0,0 +1,28 @@
+function pag() {
+    window.location.href = "index.html";
+  }
+  function user() {
+    window.location.href = "usuario.html";
+  }
+  function roupas() {
+    window.location.href = "roupas.html";
+  }
+
+const form = document.querySelector('.formphoto');
+const btn = document.querySelector('#change-photo-btn');
+
+
+
+    form.addEventListener('submit', async (e) => {
+
+        e.preventDefault();
+        
+        const data = await fetch('upload.php', {
+        method: 'POST',
+        body: new FormData(form)
+        }).then(res => res.json());
+        
+        console.log(data);
+        
+        });
+
